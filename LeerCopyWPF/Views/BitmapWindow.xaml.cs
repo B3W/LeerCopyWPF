e0c9b59bd33cb3b8fb71f930327c048154ffc647
Weston Berg <westieberg@gmail.com>
2019-02-27 20:55:19 -0600

    This view now launches as the main window to avoid using topmost property to place overlay on top

--- LeerCopyWPF/Views/BitmapWindow.xaml.cs
+++ LeerCopyWPF/Views/BitmapWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Media.Imaging;
 
 namespace LeerCopyWPF.Views
@@ -8,11 +9,44 @@ namespace LeerCopyWPF.Views
     /// </summary>
     public partial class BitmapWindow : Window
     {
-        public BitmapWindow(BitmapSource bitmap)
-        {
-            InitializeComponent();
+        /// <summary>
+        /// Bitmap of the screen to be displayed
+        /// </summary>
+        private BitmapSource bitmap;
+        /// <summary>
+        /// Guards against multiple 'Loaded' events firing
+        /// </summary>
+        private static bool loaded = false;
 
-            BitmapImg.Source = bitmap;
+        public BitmapWindow()
+        {
+            this.Initialized += BitmapWindow_Initialized;
+            this.Loaded += BitmapWindow_Loaded;
+            this.ContentRendered += BitmapWindow_ContentRendered;
+            InitializeComponent();
+        }
+
+        private void BitmapWindow_Initialized(object sender, EventArgs e)
+        {
+            bitmap = Utilities.BitmapUtilities.CaptureScreen();
+        }
+
+        private void BitmapWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!loaded)
+            {
+                BitmapImg.Source = bitmap;
+                loaded = true;
+            }
+        }
+
+        private void BitmapWindow_ContentRendered(object sender, EventArgs e)
+        {
+            Overlay overlay = new Overlay(bitmap);
+            overlay.Owner = this;
+            overlay.ShowDialog();
+
+            this.Close();
         }
     }
 }

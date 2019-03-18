b22b3befbff8f7f3ca1f612861ed9990c9ed01e1
Weston Berg <westieberg@gmail.com>
2019-03-18 00:35:36 -0500

    Enum handling valid actions during selection

---
+++ LeerCopyWPF/Enums/Actions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeerCopyWPF.Enums
+{
+    public class Actions
+    {
+        /// <summary>
+        /// All actions able to be performed during selection
+        /// </summary>
+        public enum ActionEnum
+        {
+            Invalid,
+            Copy,
+            Edit,
+            Save,
+            SelectAll,
+            Clear,
+            New,
+            Settings,
+            Tips,
+            Quit
+        }
+    }
+}

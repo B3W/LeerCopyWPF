3dedc047d3851e88bf1c51f3b123df8f32f94d07
Weston Berg <westieberg@gmail.com>
2019-03-20 12:09:36 -0500

    Added setting for AppData location

--- LeerCopyWPF/Properties/Settings.Designer.cs
+++ LeerCopyWPF/Properties/Settings.Designer.cs
@@ -177,5 +177,17 @@ namespace LeerCopyWPF.Properties {
                 this["DefaultSaveExt"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute(".")]
+        public string AppDataLoc {
+            get {
+                return ((string)(this["AppDataLoc"]));
+            }
+            set {
+                this["AppDataLoc"] = value;
+            }
+        }
     }
 }

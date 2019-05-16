f9a1f831a08098f0cfa5eb9aa28b45c21cd82afb
B3W <westieberg@gmail.com>
2019-05-15 20:12:01 -0500

    Changed from 'static readonly' to 'const'

--- LeerCopyWPF/Constants/SettingsConstants.cs
+++ LeerCopyWPF/Constants/SettingsConstants.cs
@@ -7,15 +7,15 @@ namespace LeerCopyWPF.Constants
     public static class SettingsConstants
     {
         // Key Settings
-        public static readonly string CopySettingName = "CopyKey";
-        public static readonly string EditSettingName = "EditKey";
-        public static readonly string SaveSettingName = "SaveKey";
-        public static readonly string ClearSettingName = "ClearKey";
-        public static readonly string SelectAllSettingName = "SelectAll";
-        public static readonly string BorderSettingName = "BorderKey";
-        public static readonly string TipsSettingName = "TipsKey";
-        public static readonly string SwtchScrnSettingName = "SwitchScreenKey";
-        public static readonly string SettingsSettingName = "SettingsWin";
-        public static readonly string QuitSettingName = "QuitKey";
+        public const string CopySettingName = "CopyKey";
+        public const string EditSettingName = "EditKey";
+        public const string SaveSettingName = "SaveKey";
+        public const string ClearSettingName = "ClearKey";
+        public const string SelectAllSettingName = "SelectAll";
+        public const string BorderSettingName = "BorderKey";
+        public const string TipsSettingName = "TipsKey";
+        public const string SwtchScrnSettingName = "SwitchScreenKey";
+        public const string SettingsSettingName = "SettingsWin";
+        public const string QuitSettingName = "QuitKey";
     }
 }

dcaeeef7aba09b9873f366dfef70074f3006cdfb
B3W <westieberg@gmail.com>
2021-01-05 22:26:18 -0600

    Code cleanup.

--- LeerCopyWPF/ViewModels/KeyBindingHelperViewModel.cs
+++ LeerCopyWPF/ViewModels/KeyBindingHelperViewModel.cs
@@ -8,7 +8,6 @@ namespace LeerCopyWPF.ViewModels
     public class KeyBindingHelperViewModel : BaseViewModel
     {
         #region Fields
-        protected bool _mappingsChanged;
         #endregion // Fields
 
         #region Properties
@@ -29,15 +28,9 @@ namespace LeerCopyWPF.ViewModels
         #region Methods
         public void RefreshKeyBinding(string key, string value)
         {
-            SetMapping(key, value);
-
-            if (_mappingsChanged)
+            if (SetMapping(key, value))
             {
-                if (KeyBindingsChangedEvent != null)
-                {
-                    KeyBindingsChangedEvent.Invoke(this, EventArgs.Empty);
-                    _mappingsChanged = false;
-                }
+                KeyBindingsChangedEvent?.Invoke(this, EventArgs.Empty);
             }
         } // RefreshKeyBinding
 
@@ -48,25 +41,22 @@ namespace LeerCopyWPF.ViewModels
         public void RefreshKeyBindings()
         {
             Properties.Settings settingsInst = Properties.Settings.Default;
+            bool mappingsChanged = false;
 
-            SetMapping(SettingsConstants.CopySettingName, (string)settingsInst[SettingsConstants.CopySettingName]);
-            SetMapping(SettingsConstants.EditSettingName, (string)settingsInst[SettingsConstants.EditSettingName]);
-            SetMapping(SettingsConstants.SaveSettingName, (string)settingsInst[SettingsConstants.SaveSettingName]);
-            SetMapping(SettingsConstants.ClearSettingName, (string)settingsInst[SettingsConstants.ClearSettingName]);
-            SetMapping(SettingsConstants.SelectAllSettingName, (string)settingsInst[SettingsConstants.SelectAllSettingName]);
-            SetMapping(SettingsConstants.BorderSettingName, (string)settingsInst[SettingsConstants.BorderSettingName]);
-            SetMapping(SettingsConstants.TipsSettingName, (string)settingsInst[SettingsConstants.TipsSettingName]);
-            SetMapping(SettingsConstants.SwtchScrnSettingName, (string)settingsInst[SettingsConstants.SwtchScrnSettingName]);
-            SetMapping(SettingsConstants.SettingsSettingName, (string)settingsInst[SettingsConstants.SettingsSettingName]);
-            SetMapping(SettingsConstants.QuitSettingName, (string)settingsInst[SettingsConstants.QuitSettingName]);
+            mappingsChanged |= SetMapping(SettingsConstants.CopySettingName, (string)settingsInst[SettingsConstants.CopySettingName]);
+            mappingsChanged |= SetMapping(SettingsConstants.EditSettingName, (string)settingsInst[SettingsConstants.EditSettingName]);
+            mappingsChanged |= SetMapping(SettingsConstants.SaveSettingName, (string)settingsInst[SettingsConstants.SaveSettingName]);
+            mappingsChanged |= SetMapping(SettingsConstants.ClearSettingName, (string)settingsInst[SettingsConstants.ClearSettingName]);
+            mappingsChanged |= SetMapping(SettingsConstants.SelectAllSettingName, (string)settingsInst[SettingsConstants.SelectAllSettingName]);
+            mappingsChanged |= SetMapping(SettingsConstants.BorderSettingName, (string)settingsInst[SettingsConstants.BorderSettingName]);
+            mappingsChanged |= SetMapping(SettingsConstants.TipsSettingName, (string)settingsInst[SettingsConstants.TipsSettingName]);
+            mappingsChanged |= SetMapping(SettingsConstants.SwtchScrnSettingName, (string)settingsInst[SettingsConstants.SwtchScrnSettingName]);
+            mappingsChanged |= SetMapping(SettingsConstants.SettingsSettingName, (string)settingsInst[SettingsConstants.SettingsSettingName]);
+            mappingsChanged |= SetMapping(SettingsConstants.QuitSettingName, (string)settingsInst[SettingsConstants.QuitSettingName]);
 
-            if (_mappingsChanged)
+            if (mappingsChanged)
             {
-                if (KeyBindingsChangedEvent != null)
-                {
-                    KeyBindingsChangedEvent.Invoke(this, EventArgs.Empty);
-                    _mappingsChanged = false;
-                }
+                KeyBindingsChangedEvent?.Invoke(this, EventArgs.Empty);
             }
         } // RefreshKeyBindings
 
@@ -76,21 +66,26 @@ namespace LeerCopyWPF.ViewModels
         /// </summary>
         /// <param name="key"></param>
         /// <param name="value"></param>
-        private void SetMapping(string key, string value)
+        /// <returns>true if mapping changed, false otherwise</returns>
+        private bool SetMapping(string key, string value)
         {
+            bool mappingChanged = false;
+
             if (KeyMappings.TryGetValue(key, out string curValue))
             {
                 if (value != curValue)
                 {
                     KeyMappings[key] = value;
-                    _mappingsChanged = true;
+                    mappingChanged = true;
                 }
             }
             else
             {
                 KeyMappings.Add(key, value);
-                _mappingsChanged = true;
+                mappingChanged = true;
             }
+
+            return mappingChanged;
         } // SetMapping
         #endregion // Methods
     }

7b07b43f0ce7480b5b2c37a9a71c45cf9039859a
B3W <westieberg@gmail.com>
2021-02-21 20:09:21 -0600

    Added generic implementation of ICommand interface for passing typed argument to command.

--- LeerCopyWPF/Commands/RelayCommand.cs
+++ LeerCopyWPF/Commands/RelayCommand.cs
@@ -6,9 +6,9 @@ using System.Windows.Input;
 namespace LeerCopyWPF.Commands
 {
     /// <summary>
-    /// ICommand implementation that ensures WPF queries ready status of
-    /// RelayCommands when they query ready status of built-in commands by
-    /// using CommandManager.RequerySuggested.
+    /// Non-generic ICommand implementation that ensures WPF queries ready
+    /// status of RelayCommands when they query ready status of built-in
+    /// commands by using CommandManager.RequerySuggested.
     /// </summary>
     public class RelayCommand : ICommand
     {
@@ -37,16 +37,6 @@ namespace LeerCopyWPF.Commands
             remove { CommandManager.RequerySuggested -= value; }
         }
 
-        public bool CanExecute(object parameter)
-        {
-            return _canExecute == null || _canExecute(parameter);
-        }
-
-        public void Execute(object parameter)
-        {
-            _execute(parameter);
-        }
-
         #endregion // ICommand Members
 
         #endregion // Properties
@@ -61,6 +51,85 @@ namespace LeerCopyWPF.Commands
             _execute = execute ?? throw new ArgumentNullException("execute", "Command's Action cannot be null");
             _canExecute = canExecute;
         }
+
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        #endregion // ICommand Members
+
+        #endregion // Methods
+    }
+
+    /// <summary>
+    /// Generic ICommand implementation that ensures WPF queries ready
+    /// status of RelayCommands when they query ready status of built-in
+    /// commands by using CommandManager.RequerySuggested.
+    /// </summary>
+    public class RelayCommand<T> : ICommand
+    {
+        #region Fields
+
+        /// <summary>
+        /// Logic to run when command executed
+        /// </summary>
+        private readonly Action<T> _execute;
+
+        /// <summary>
+        /// Logic for determining if command is able to be executed
+        /// </summary>
+        private readonly Predicate<object> _canExecute;
+
+        #endregion // Fields
+
+
+        #region Properties
+
+        #region ICommand Members
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        #endregion // ICommand Members
+
+        #endregion // Properties
+
+
+        #region Methods
+
+        public RelayCommand(Action<T> execute) : this(execute, null) { }
+
+        public RelayCommand(Action<T> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute", "Command's Action cannot be null");
+            _canExecute = canExecute;
+        }
+
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            T castParameter = (T)Convert.ChangeType(parameter, typeof(T));
+            _execute(castParameter);
+        }
+
+        #endregion // ICommand Members
 
         #endregion // Methods
     }

df67122e7736e79f08155cc8a9b8dc286704b680
B3W <westieberg@gmail.com>
2021-03-05 21:41:06 -0600

    Moved all settings constants into this class

--- LeerCopyWPF/Constants/SettingsConstants.cs
+++ LeerCopyWPF/Constants/SettingsConstants.cs
@@ -6,16 +6,79 @@ namespace LeerCopyWPF.Constants
 {
     public static class SettingsConstants
     {
-        // Key Settings
+        /// <summary>
+        /// 'Copy selection' action key binding setting
+        /// </summary>
         public const string CopySettingName = "CopyKey";
+
+        /// <summary>
+        /// 'Edit selection' action key binding setting
+        /// </summary>
         public const string EditSettingName = "EditKey";
+
+        /// <summary>
+        /// 'Save selection' action key binding setting
+        /// </summary>
         public const string SaveSettingName = "SaveKey";
+
+        /// <summary>
+        /// 'Clear selection' action key binding setting
+        /// </summary>
         public const string ClearSettingName = "ClearKey";
+
+        /// <summary>
+        /// 'Select all' action key binding setting
+        /// </summary>
         public const string SelectAllSettingName = "SelectAll";
+
+        /// <summary>
+        /// 'Toggle border' action key binding setting
+        /// </summary>
         public const string BorderSettingName = "BorderKey";
+
+        /// <summary>
+        /// 'Toggle tips' action key binding setting
+        /// </summary>
         public const string TipsSettingName = "TipsKey";
-        public const string SwtchScrnSettingName = "SwitchScreenKey";
+        
+        /// <summary>
+        /// 'Open settings' action key binding setting
+        /// </summary>
         public const string SettingsSettingName = "SettingsWin";
+
+        /// <summary>
+        /// 'Quit selection' action key binding setting
+        /// </summary>
         public const string QuitSettingName = "QuitKey";
+
+        /// <summary>
+        /// Property name for default file extension setting
+        /// </summary>
+        public const string ConstFileExtPropName = "DefaultSaveExt";
+
+        /// <summary>
+        /// Property name for default file name setting
+        /// </summary>
+        public const string ConstFileNamePropName = "DefaultFileName";
+
+        /// <summary>
+        /// Property name for border visibility setting
+        /// </summary>
+        public const string ConstSelectBorderVisPropName = "BorderVisibility";
+
+        /// <summary>
+        /// Property name for tips visibility setting
+        /// </summary>
+        public const string ConstTipsVisPropName = "TipsVisibility";
+
+        /// <summary>
+        /// Property name for window opacity setting
+        /// </summary>
+        public const string ConstSelectWinOpacityPropName = "SelectionWinOpacity";
+
+        /// <summary>
+        /// Max opacity for the selection window's background
+        /// </summary>
+        public const double ConstSelectOpacityMax = 0.3;
     }
 }

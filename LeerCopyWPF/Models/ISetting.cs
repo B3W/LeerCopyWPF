71492350083cea6f38489acf183f94fa802e4d39
B3W <westieberg@gmail.com>
2021-03-05 21:40:18 -0600

    Added generic class encapsulating an application setting. Added interface that application setting must implement.

---
+++ LeerCopyWPF/Models/ISetting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeerCopyWPF.Models
+{
+    public interface ISetting
+    {
+        /// <summary>
+        /// Unique identifier for setting
+        /// </summary>
+        string SettingID { get; }
+
+        /// <summary>
+        /// Flag indicating if the setting's value has changed
+        /// </summary>
+        bool ValueModified { get; }
+
+        /// <summary>
+        /// The result of the latest 'Validate' call
+        /// </summary>
+        bool IsValid { get; }
+
+        /// <summary>
+        /// Error message describing why setting is in invalid or empty if setting valid
+        /// </summary>
+        string ErrorMessage { get; set; }
+
+        /// <summary>
+        /// Validates the setting
+        /// </summary>
+        /// <returns>true if valid, false otherwise</returns>
+        bool Validate();
+
+        /// <summary>
+        /// Saves the setting
+        /// </summary>
+        /// <returns>true on success, false otherwise</returns>
+        bool Save();
+    }
+}
---
+++ LeerCopyWPF/Models/Setting.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeerCopyWPF.Models
+{
+    public class Setting<T> : ISetting where T : IEquatable<T>
+    {
+        #region Fields
+
+        #region Public Fields
+        #endregion
+
+        #region Protected Fields
+        #endregion
+
+        #region Private Fields
+
+        private readonly Predicate<Setting<T>> _validationFunc;
+
+        #endregion
+
+        #endregion // Fields
+
+
+        #region Properties
+
+        #region Public Properties
+
+        public T OriginalValue { get; private set; }
+
+        public T CurrentValue { get; set; }
+
+        public string SettingID { get; }
+
+        public bool ValueModified { get => !OriginalValue.Equals(CurrentValue); }
+
+        public bool IsValid { get; private set; }
+
+        public string ErrorMessage { get; set; }
+
+        #endregion
+
+        #region Protected Properties
+        #endregion
+
+        #region Private Properties
+        #endregion
+
+        #endregion // Properties
+
+
+        #region Fields
+
+        #region Public Methods
+
+        public Setting(string settingId, T originalValue) : this(settingId, originalValue, null) { }
+
+        public Setting(string settingId, T originalValue, Predicate<Setting<T>> validationFunc)
+        {
+            _validationFunc = validationFunc;
+
+            SettingID = settingId;
+            OriginalValue = originalValue;
+            CurrentValue = originalValue;
+        }
+
+        public bool Validate()
+        {
+            bool isValid = true;
+
+            if (_validationFunc != null)
+            {
+                isValid = _validationFunc(this);
+            }
+
+            IsValid = isValid;
+
+            return isValid;
+        }
+
+        public bool Save()
+        {
+            Properties.Settings.Default[SettingID] = CurrentValue;
+            Properties.Settings.Default.Save();
+
+            OriginalValue = CurrentValue;
+
+            return true;
+        }
+
+        #endregion
+
+        #region Protected Methods
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #endregion // Methods
+    }
+}

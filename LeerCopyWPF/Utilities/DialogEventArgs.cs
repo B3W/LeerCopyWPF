b04725ab41e9463ff80363270de6079533182210
B3W <westieberg@gmail.com>
2021-03-30 21:51:35 -0500

    Moved DialogEventArgs into shared location.

--- LeerCopyWPF/Utilities/DialogEventArgs.cs
+++ LeerCopyWPF/Utilities/DialogEventArgs.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LeerCopyWPF.Controller
+namespace LeerCopyWPF.Utilities
 {
     public class DialogEventArgs : EventArgs
     {
